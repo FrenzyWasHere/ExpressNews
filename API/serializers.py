fe21f186093ffb9b712e677b633f69d3ef5b13f7
FrenzyWasHere <108968705+FrenzyWasHere@users.noreply.github.com>
2025-01-19 14:49:39 +0545

    Update serializers.py

--- API/serializers.py
+++ API/serializers.py
@@ -16,7 +16,11 @@ class UserRegistrationSerializer(ModelSerializer):
         fields = ['username', 'email', 'password']
         
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
 
 
